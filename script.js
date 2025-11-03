bd83a241361bd04cf203ff13ffce693968b9a639
Rakesh07778777 <bossrakesh43@gmail.com>
2025-11-03 15:37:35 +0530

    gsap

--- script.js
+++ script.js
@@ -10,5 +10,15 @@ window.addEventListener('wheel', (scrolling) => {
         gsap.to('.texts img', {
             rotate: 180
         })
-    } 
+    } else{
+         gsap.to('.page-image', {
+            transform: 'translateX(0%)',
+            duration: 2,
+            repeat: -1,
+            ease: 'none'
+        })
+            gsap.to('.texts img', {
+            rotate: 0
+        })
+    }
 })
